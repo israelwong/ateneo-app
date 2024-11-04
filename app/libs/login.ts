02c9d372f1e3057388d75b808f9efc6f4cdee321
Israel Wong <israelwong@iMac-de-Israel.local>
2024-11-04 12:34:58 -0600

    update status user

--- app/libs/login.ts
+++ app/libs/login.ts
@@ -7,30 +7,33 @@ import jwt from 'jsonwebtoken';
 const prisma = new PrismaClient();
 
 export default async function authenticateUser(email: string, password: string) {
-    try{
+    try {
 
         const res: {
             token: string;
             error: string;
         } = { token: '', error: '' };
-        
+
         // Busca el usuario en la base de datos
         const user = await prisma.user.findUnique({
-            where: { email },
+            where: {
+                email,
+                status: 'Active'
+            },
         });
-        
+
         // Verifica si el usuario existe y la contraseña es correcta
         if (!user || !(await bcrypt.compare(password, user.password))) {
             return res.token
         }
-        
+
         // Crear JWT
         const secret = process.env.JWT_SECRET;
         if (!secret) {
             console.log('JWT_SECRET no está definido en las variables de entorno');
             return res.token
         }
-        
+
         // Crea token
         const token = jwt.sign(
             {
@@ -43,7 +46,7 @@ export default async function authenticateUser(email: string, password: string) 
             }
         );
         res.token = token;
-        
+
         // Devuelve el token
         return res
     } catch (error: unknown) {
